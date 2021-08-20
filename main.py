fe80949c121dbb1c7545dc8b8634bd5f4efb5240
RussellDash332 <russellsaerang@gmail.com>
2021-08-20 22:22:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -15,12 +15,20 @@ load_dotenv()
 TOKEN = os.getenv('TOKEN')
 
 @client.event
+async def on_ready():
+    await client.change_presence(
+        activity=discord.Game(name="rs help", type=3))
+    print("Status is set!")
+
+@client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith("rs"):
-        response = chatbot.request(message.content[7:])
+    if message.content == "rs help":
+        await message.channel.send("**Help!**\nType \"rs <sentence>\" to start chatting!\nExample: rs Hello, how are you?")
+    elif message.content.startswith("rs"):
+        response = chatbot.request(message.content[3:])
         await message.channel.send(response)
 
 client.run(TOKEN)
